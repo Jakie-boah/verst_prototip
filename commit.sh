7618aef51f91f984dee95406881e5bbe33c26bcb
NAME <VanoGalen@yandex.ru>
2023-02-15 15:40:39 +0300

    admin_panel req

--- commit.sh
+++ commit.sh
@@ -3,7 +3,7 @@
 git config --global user.name 'NAME'
 git config --global user.email 'VanoGalen@yandex.ru'
 git add *
-git commit -m "admin_panel"
+git commit -m "admin_panel req"
 git branch -M main
 git remote add origin https://github.com/Jakie-boah/verst_prototip.git
 git push -u origin main
